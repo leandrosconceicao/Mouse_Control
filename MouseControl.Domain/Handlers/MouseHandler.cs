1b6b5d9a198facf713cbc00be17d21af134c5f40
Leandro <leandrosantosconceicao@gmail.com>
2025-01-05 21:27:10 -0300

    Implementando handler de gerenciamento do mouse

---
+++ MouseControl.Domain/Handlers/MouseHandler.cs
@@ -0,0 +1,31 @@
+﻿using MouseControl.Domain.Interfaces;
+using System.Runtime.InteropServices;
+
+namespace MouseControl.Domain.Handlers
+{
+    public sealed class MouseHandler : IMouseHandler
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public void MouseLeftClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+
+        public void MouseRightClick()
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+        }
+
+        public bool SetCursor(int x, int y) => SetCursorPos(x, y);
+    }
+}
---
+++ MouseControl.Domain/Interfaces/IMouseHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MouseControl.Domain.Interfaces
+{
+    public interface IMouseHandler
+    {
+        public bool SetCursor(int x, int y);
+
+        public void MouseLeftClick();
+
+        public void MouseRightClick();
+    }
+}
