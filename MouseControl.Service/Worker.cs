7c8750680001b0a3c5d3707b953e8f85bc615222
Leandro <leandrosantosconceicao@gmail.com>
2025-01-05 21:28:16 -0300

    Refatorando código para utilizar interface de controle do mouse

--- MouseControl.Service/Worker.cs
+++ MouseControl.Service/Worker.cs
@@ -1,16 +1,18 @@
 using Microsoft.AspNetCore.SignalR.Client;
 using MouseControl.Domain.Entities;
-using System.Runtime.InteropServices;
+using MouseControl.Domain.Interfaces;
 
 namespace MouseControl.Service
 {
     public class Worker : BackgroundService
     {
         private readonly IConfiguration _config;
+        private readonly IMouseHandler _mouseHandler;
 
-        public Worker(IConfiguration configuration)
+        public Worker(IConfiguration configuration, IMouseHandler mouseHandler)
         {
             _config = configuration;
+            _mouseHandler = mouseHandler;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -33,19 +35,19 @@ namespace MouseControl.Service
                             connection.On<MousePosition>("SetPosition", position =>
                             {
                                 Console.WriteLine($"Posição do mouse {position}");
-                                SetCursorPos(Convert.ToInt32(position.X), Convert.ToInt32(position.Y));
+                                _mouseHandler.SetCursor(Convert.ToInt32(position.X), Convert.ToInt32(position.Y));
                             });
 
                             connection.On("LeftClick", () =>
                             {
                                 Console.WriteLine("Botão esquerdo do mouse clicado");
-                                MouseLeftClick();
+                                _mouseHandler.MouseLeftClick();
                             });
 
                             connection.On("RightClick", () =>
                             {
                                 Console.WriteLine("Botão direito do mouse clicado");
-                                MouseRightClick();
+                                _mouseHandler.MouseRightClick();
                             });
                             break;
 
@@ -61,27 +63,6 @@ namespace MouseControl.Service
                 }
                 await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
             }
-        }
-
-        [DllImport("user32.dll")]
-        static extern bool SetCursorPos(int X, int Y);
-
-        [DllImport("user32.dll")]
-        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
-
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
-        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
-        private const int MOUSEEVENTF_RIGHTUP = 0x10;
-
-        private void MouseLeftClick()
-        {
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-        }
-
-        private void MouseRightClick()
-        {
-            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
         }
     }
 }
